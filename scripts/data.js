6042929327d5aa3e62f06f337fd3a64dea35e4f3
luthfidhani <luthfidhani17@gmail.com>
2024-10-03 21:41:45 +0700

    first commit

---
+++ scripts/data.js
@@ -0,0 +1,24 @@
+const formLinks = {
+    forms: [
+        {
+            title: "📅 Pengeluaran Bulanan dan Langganan",
+            link: "https://forms.gle/RivYJpCHrEPKoNcq6"
+        },
+        {
+            title: "💰 Simpanan",
+            link: "https://forms.gle/fSavk2DUaWA9U7jT6"
+        },
+        {
+            title: "🤑 Pemasukan",
+            link: "https://forms.gle/NRmN5hisr3S4C8536"
+        },
+        {
+            title: "💸 Pengeluaran",
+            link: "https://forms.gle/nXuET9HmLjxZs8j5A"
+        },
+        {
+            title: "📊 Dashboard",
+            link: "https://docs.google.com/spreadsheets/d/1dFfqRT4llj58XH25xjrpRPxgMGBR_DJaFBTqKmqaW80/edit?usp=sharing"
+        }
+    ]
+}
